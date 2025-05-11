ac5290db066f30dab4f01e6b7604a9fe29cd08d3
arijustech <arijus.ka@outlook.com>
2025-05-11 15:22:56 +0300

    update

--- install.sh
+++ install.sh
@@ -16,30 +16,17 @@ install_packages() {
   # Arch Linux (Pacman)
   if [[ "$DISTRO" == "arch" || "$DISTRO" == "manjaro" ]]; then
     echo "Arch Linux or Manjaro detected, installing packages with pacman."
-    sudo pacman -S --needed --noconfirm pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber pavucontrol easyeffects lv2 lilv suil lsp-plugins calf zam-plugins x42-plugins dragonfly-reverb carla qjackctl realtime-privileges
+    sudo pacman -S --needed --noconfirm pipewire pipewire-pulse pipewire-alsa pipewire-jack pipewire-v4l2 wireplumber pavucontrol easyeffects lv2 lilv suil lsp-plugins calf zam-plugins x42-plugins dragonfly-reverb carla qjackctl realtime-privileges ladspa ladspa-plugins
     sudo usermod -aG realtime $USER
 
   # Debian/Ubuntu (APT)
   elif [[ "$DISTRO" == "debian" || "$DISTRO" == "ubuntu" || "$DISTRO" == "linuxmint" ]]; then
     echo "Debian/Ubuntu or Linux Mint detected, installing packages with apt."
     sudo apt update -y
-    sudo apt install -y pipewire pipewire-audio-client-libraries pipewire-jack wireplumber pavucontrol easyeffects lv2 lilv suil lsp-plugins calf zam-plugins x42-plugins dragonfly-reverb carla qjackctl realtime-privileges
+    sudo apt install -y pipewire pipewire-audio-client-libraries pipewire-jack pipewire-v4l2 wireplumber pavucontrol easyeffects lv2 lilv suil lsp-plugins calf zam-plugins x42-plugins dragonfly-reverb carla qjackctl realtime-privileges ladspa ladspa-plugins
     sudo usermod -aG realtime $USER
 
   # Fedora/RHEL (DNF)
   elif [[ "$DISTRO" == "fedora" || "$DISTRO" == "rhel" || "$DISTRO" == "centos" ]]; then
     echo "Fedora/RHEL or CentOS detected, installing packages with dnf."
-    sudo dnf install -y pipewire pipewire-pulseaudio pipewire-jack wireplumber pavucontrol easyeffects lv2 lilv suil lsp-plugins calf zam-plugins x42-plugins dragonfly-reverb carla qjackctl realtime-privileges
-    sudo usermod -aG realtime $USER
-
-  else
-    echo "Unsupported distribution detected: $DISTRO"
-    exit 1
-  fi
-}
-
-# Skambinama funkcija, kad įdiegtų paketai priklausomai nuo distros
-install_packages
-
-echo "Installation completed. Please log out and log back in to apply group changes."
-echo "For all changes to take effect, please reboot your computer."
+    sudo dnf
